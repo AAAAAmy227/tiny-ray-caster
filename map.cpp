acebbd6535ecf8ccf9c63badf0e84d232f2527e8
Ledner <faq24@mails.tsinghua.edu.cn>
2024-12-16 14:37:16 +0800

    add some "const" and change the color of map

--- map.cpp
+++ map.cpp
@@ -4,12 +4,13 @@
 #include<vector>
 #include<string>
 #include<cassert>
-uint32_t pack_colors(uint8_t r, uint8_t g, uint8_t b, uint8_t a = 255)
+//a: alpha, transparency
+uint32_t pack_colors(const uint8_t r, const uint8_t g, const uint8_t b, const uint8_t a = 255)
 {
     return (a << 24) + (b << 16) + (g << 8) + r; // 0xAABBGGRR 
 }
 
-void unpack_colors(uint32_t color, uint8_t& r, uint8_t& g, uint8_t& b, uint8_t& a)
+void unpack_colors(const uint32_t color, uint8_t& r, uint8_t& g, uint8_t& b, uint8_t& a)
 {
     r = (color >> 0)&255;
     g = (color >> 8)&255;
@@ -17,7 +18,7 @@ void unpack_colors(uint32_t color, uint8_t& r, uint8_t& g, uint8_t& b, uint8_t& 
     a = (color >> 24)&255;
 }
 
-void drop_ppm(std::string filename,std::vector<uint32_t> buffer,uint16_t w, uint16_t h)
+void drop_ppm(std::string filename,const std::vector<uint32_t>& buffer,uint16_t w, uint16_t h)
 {
     assert(buffer.size()==w*h);
     std::ofstream ofs(filename,std::ios::binary);
@@ -32,11 +33,11 @@ void drop_ppm(std::string filename,std::vector<uint32_t> buffer,uint16_t w, uint
 int main()
 {
     std::vector<uint32_t> map;
-    uint16_t w=512,h=512;
+    const size_t w=512,h=512; //idk why but the tutorial uses size_t
     for(uint16_t i=0;i<w;i++)
     for(uint16_t j=0;j<h;j++)
     {
-        map.push_back(pack_colors(255*static_cast<float>(i/w),255*static_cast<float>(j/h),255*static_cast<float>(i*j/(w*h))));
+        map.push_back(pack_colors(255*i/float(w),0,255*j/float(h)));
     }
-    drop_ppm("test.ppm",map,w,h);
+    drop_ppm("./test.ppm",map,w,h); //add path here
 }
